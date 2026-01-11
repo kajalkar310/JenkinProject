68735e8fe0506bd31c89186eb23f205976f11688
Java code <javacodingbyme@gmail.com>
2026-01-11 13:34:15 +0530

    Changes for UTC

---
+++ src/test/java/com/example/user/controller/UserControllerTest.java
@@ -0,0 +1,57 @@
+package com.example.user.controller;
+
+import com.example.user.entity.User;
+import com.example.user.repository.UserRepository;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+import java.util.Collections;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
+
+
+@WebMvcTest(UserController.class)
+class UserControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private UserRepository repository;
+
+
+    @Test
+    void testGetAllUsers() throws Exception{
+        // Implement test logic here
+        List<User> users= List.of(
+                new User (1L, "John Doe", "john@gmail.com"),
+                new User(2L, "Jane Smith", "john@gmail.com")
+        );
+        when(repository.findAll()).thenReturn(users);
+        mockMvc.perform(get("/users"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.length()").value(users.size()));
+
+
+    }
+
+
+    @Test
+    void getAllUsers_whenNoUsers_thenReturnEmptyList() throws Exception {
+        when(repository.findAll()).thenReturn(Collections.emptyList());
+
+        mockMvc.perform(get("/users"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.length()").value(0));
+    }
+
+}
