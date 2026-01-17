c74a187186b601901a08b48fc616801173679cd8
Java code <javacodingbyme@gmail.com>
2026-01-17 16:17:23 +0530

    Reapply "Merge pull request #4 from kajalkar310/Auth-login"
    This reverts commit 2c3d72b79ecfb3f5aecd79c063dcc2cbc70fc517.

--- src/main/java/com/example/user/UserServiceApplication.java
+++ src/main/java/com/example/user/UserServiceApplication.java
@@ -2,10 +2,20 @@ package com.example.user;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
 public class UserServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(UserServiceApplication.class, args);
+        System.out.println(new BCryptPasswordEncoder().encode("admin123"));
+
+        BCryptPasswordEncoder   encoder=new BCryptPasswordEncoder();
+        boolean matches =
+
+        encoder.matches("admin123", "$2a$10$mIkx3KFyBo/W5BEnObAzKeiv0cUNH5R20iw32O9wCJnWfKRmFXu1e");
+
+
+            System.out.println(" matches password :"+matches);
     }
 }
