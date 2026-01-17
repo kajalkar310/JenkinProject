87289811f89d992833b881385fab06c44beac3b3
Java code <javacodingbyme@gmail.com>
2026-01-17 13:27:36 +0530

    swagger configuration changes

---
+++ src/main/java/com/example/user/config/SwaggerConfig.java
@@ -0,0 +1,33 @@
+package com.example.user.config;
+
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        SecurityScheme securityScheme = new SecurityScheme()
+                .type(SecurityScheme.Type.HTTP)
+                .scheme("bearer")
+                .bearerFormat("JWT");
+
+        return new OpenAPI()
+                .info(new Info()
+                        .title("User Mapping API")
+                        .version("1.0")
+                        .description("Swagger with Authentication"))
+                .addSecurityItem(new SecurityRequirement().addList("bearerAuth"))
+                .components(new Components()
+                        .addSecuritySchemes("bearerAuth", securityScheme));
+
+
+    }
+}
